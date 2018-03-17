a8440a070395e794b869195668cd27cee266a033
Jean-Louis Fuchs <ganwell@fangorn.ch>
2018-03-17 16:52:07 +0100

    Release 1.0.4b0

--- setup.py
+++ setup.py
@@ -21,7 +21,73 @@ try:
         name="libchirp",
         version=__version__,
         description="Message-passing for everyone.",
-        long_description=open("README.rst", "rt").read(),
+        long_description="""
+============
+python-chirp
+============
+
+Message-passing for everyone
+
+BETA-RELEASE: 1.0.0
+===================
+
+https://github.com/concretecloud/python-chirp
+
+Features
+========
+
+* Fully automatic connection setup
+
+* TLS support
+
+  * Connections to 127.0.0.1 and ::1 aren't encrypted
+
+* Easy message routing
+
+* Robust
+
+  * No message can be lost without an error (in sync mode)
+
+* Very thin API
+
+* Minimal code-base, all additional features will be implemented as modules in
+  an upper layer
+
+* Fast
+
+Install
+=======
+
+Dependencies
+------------
+
+.. code-block:: bash
+
+   Alpine:       apk add python3-dev libffi-dev libressl-dev libuv-dev
+   Debian-based: apt install python3-dev libffi-dev libssl-dev libuv1-dev
+   Redhat-based: yum install python3-devel libffi-devel openssl-devel
+                 libuv-devel
+   Arch:         pacman -S libffi openssl libuv
+   OSX:          brew install libffi openssl libuv
+
+pip
+---
+
+If we have wheels for your platform, you don't need to install any
+dependencies.
+
+.. code-block:: bash
+
+   pip install libchirp
+
+setup.py
+--------
+
+.. code-block:: bash
+
+   pip install cffi
+   python setup.py install
+        """,
         url="https://github.com/concretecloud/python-chirp",
         author="Jean-Louis Fuchs",
         author_email="ganwell@fangorn.ch",
