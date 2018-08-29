8e648d6cd5c403fb50fa4884f23aa347bffdf60d
Jean-Louis Fuchs <ganwell@fangorn.ch>
2018-08-29 21:24:45 +0200

    Caplog was fixed long ago: back to latest pytest

--- ci/osx.sh
+++ ci/osx.sh
@@ -5,7 +5,7 @@ PKGS="python3 libuv libffi openssl"
 brew update > /dev/null
 brew install $PKGS
 brew upgrade $PKGS
-pip3 install cffi 'pytest<3.4' hypothesis  # pytest: currently caplog fails
+pip3 install cffi pytest hypothesis  # pytest: currently caplog fails
 python3 libchirp_cffi.py
 clang echo_test.c libchirp.c \
     -luv -lssl -lcrypto -lm -lpthread -o echo_test -Os -DNDEBUG \
