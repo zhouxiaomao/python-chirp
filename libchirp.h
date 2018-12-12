f2dc7083af7ff42cba0d9fc77ba41cd82300062e
Jean-Louis Fuchs <ganwell@fangorn.ch>
2018-12-12 13:11:56 +0100

    Release 1.2.1b0

--- libchirp.h
+++ libchirp.h
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // ================================
-// libchirp 1.2.0-beta amalgamation
+// libchirp 1.2.1-beta amalgamation
 // ================================
 
 // =========
--- libchirp/version.py
+++ libchirp/version.py
@@ -1,3 +1,3 @@
 """Version."""
 
-__version__ = "1.2.0b0"
+__version__ = "1.2.1b0"
